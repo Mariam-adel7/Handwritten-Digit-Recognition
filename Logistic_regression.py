d2da210e9d421a1cb5dd8ac556096286a0f49b34
Maryouma <126493222+Mariam-adel7@users.noreply.github.com>
2025-12-20 22:25:00 +0200

    Update Logistic_regression.py

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -1,9 +1,7 @@
 import numpy as np
-from pre_processing import X_train, X_test, y_train_labels, y_test_labels
+from preprocessing import X_train, X_test, y_train, y_test, pca
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import random
 
@@ -11,28 +9,27 @@ print("Training Logistic Regression...")
 
 model = LogisticRegression(
     solver='lbfgs',
-    multi_class='multinomial',
-    max_iter=500,
+    max_iter=1000, 
     random_state=42
 )
 
-model.fit(X_train, y_train_labels)
+model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-
-acc = accuracy_score(y_test_labels, y_pred)
-
-idx = random.randint(0, X_test.shape[0] - 1)
-img = X_test[idx].reshape(28, 28)
+acc = accuracy_score(y_test, y_pred)
 
 print("\n==============================")
 print(f"Logistic Regression Test Accuracy: {acc*100:.2f}%")
 print("==============================\n")
 
-print("Predicted:", y_pred[idx])
-print("True:     ", y_test_labels[idx])
+idx = random.randint(0, X_test.shape[0] - 1)
+img = X_test[idx]
 
-plt.imshow(img, cmap='gray')
-plt.title(f"Predicted: {y_pred[idx]} | True: {y_test_labels[idx]}")
+img_original = pca.inverse_transform(img).reshape(28, 28)
+
+print(f"Predicted: {y_pred[idx]} | True: {y_test[idx]}\n")
+
+plt.imshow(img_original, cmap='gray')
+plt.title(f"Predicted: {y_pred[idx]} | True: {y_test[idx]}")
 plt.axis('off')
 plt.show()
