8b59fab704038069196fc2adbdf738ad912a6e53
Maryouma <126493222+Mariam-adel7@users.noreply.github.com>
2025-12-20 22:25:21 +0200

    Update Neural_network.py

--- Neural_network.py
+++ Neural_network.py
@@ -1,40 +1,32 @@
-from pre_processing import X_train, X_test, y_train, y_test
+from preprocessing import X_train, X_test, y_train, y_test,pca
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score, classification_report
-import numpy as np
+from sklearn.metrics import accuracy_score
 import random
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 print("Training Neural Network...")
 
 model = MLPClassifier(
-    hidden_layer_sizes=(256, 128, 64),
+    hidden_layer_sizes=(128,64), 
     activation='relu',
     solver='adam',
-    learning_rate_init=0.001,
-    max_iter=40,
-    random_state=40
+    alpha=0.001,
+    max_iter=1000, 
+    random_state=42,
+    early_stopping=True
 )
 
 model.fit(X_train, y_train)
-
 y_pred = model.predict(X_test)
-
 acc = accuracy_score(y_test, y_pred)
 
+print(f"\nNeural Network Test Accuracy: {acc*100:.2f}%")
+
 idx = random.randint(0, X_test.shape[0] - 1)
-img = X_test[idx].reshape(28, 28)
+img = X_test[idx]
+img_original = pca.inverse_transform(img).reshape(28, 28)
 
-print("\n==============================")
-print(f"Neural Network Test Accuracy: {acc*100:.2f}%")
-print("==============================\n")
-print(f"Predicted: {y_pred[idx]} | True: {y_test[idx]}")
-
-plt.imshow(img, cmap='gray')
+plt.imshow(img_original, cmap='gray')
 plt.title(f"Predicted: {y_pred[idx]} | True: {y_test[idx]}")
 plt.axis('off')
 plt.show()
-
-
