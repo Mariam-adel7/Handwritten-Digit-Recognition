249dbc1dcb1eba82b562e21d6ba84a8b32c200c5
Maryouma <126493222+Mariam-adel7@users.noreply.github.com>
2025-12-20 22:25:42 +0200

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -2,31 +2,33 @@ import os
 import numpy as np
 from PIL import Image
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import  OneHotEncoder
+from sklearn.decomposition import PCA
 
-data_dir = "images"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(BASE_DIR, "images")
 image_size = (28, 28)
 
 X = []
-Y = []
+y = []   
 
 for digit in range(10):
     folder = os.path.join(data_dir, str(digit))
     for filename in os.listdir(folder):
         img_path = os.path.join(folder, filename)
-        try:
-            img = Image.open(img_path).convert('L')
-            img = img.resize(image_size)
-            X.append(np.array(img).flatten() / 255.0)
-            Y.append(digit)
-        except:
-            print(f"Error loading image: {img_path}")
+        img = Image.open(img_path).convert('L')
+        img = img.resize(image_size)
+        X.append(np.array(img).flatten())
+        y.append(digit)
 
 X = np.array(X)
-Y = np.array(Y)
+y = np.array(y)
+X = X / 255.0
 
-ohe = OneHotEncoder()
-Y_encoded = ohe.fit_transform(Y.reshape(-1,1)).toarray()
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
 
-X_train, X_test, y_train, y_test, y_train_labels, y_test_labels = train_test_split(
-    X, Y_encoded, Y, test_size=0.2, random_state=50)
+pca = PCA(n_components=0.95)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
