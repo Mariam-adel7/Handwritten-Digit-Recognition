ffbc4b31eb249e2ffe9f72f12b021df0f218ed57
Maryouma <126493222+Mariam-adel7@users.noreply.github.com>
2026-01-16 22:31:38 +0200

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -28,9 +28,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-pca = PCA(n_components=0.95)
+pca = PCA(n_components=300)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
+
 
 
 
