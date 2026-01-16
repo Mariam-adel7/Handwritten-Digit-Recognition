4eaf210fd1f26a21687c6a5be013c4f5d141a4c3
Maryouma <126493222+Mariam-adel7@users.noreply.github.com>
2026-01-16 22:30:53 +0200

    Update Neural_network.py

--- Neural_network.py
+++ Neural_network.py
@@ -2,21 +2,23 @@ from preprocessing import X_train, X_test, y_train, y_test,pca
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 import random
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 print("Training Neural Network...")
 
 model = MLPClassifier(
-    hidden_layer_sizes=(256,128,64),  
+    hidden_layer_sizes=(256,128,64),
     activation='relu',
     learning_rate='adaptive',
-    learning_rate_init=0.001,        
-    max_iter=1500,                     
-    batch_size=64,                   
+    learning_rate_init=0.001,
+    alpha=0.001,
+    max_iter=1500,
+    batch_size=32,
     random_state=42,
     early_stopping=True
 )
-
 
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -26,10 +28,9 @@ print(f"\nNeural Network Test Accuracy: {acc*100:.2f}%")
 
 idx = random.randint(0, X_test.shape[0] - 1)
 img = X_test[idx]
-img_original = pca.inverse_transform(img).reshape(28, 28)
+img_original = pca.inverse_transform(img.reshape(1, -1)).reshape(28, 28)
 
 plt.imshow(img_original, cmap='gray')
 plt.title(f"Predicted: {y_pred[idx]} | True: {y_test[idx]}")
 plt.axis('off')
 plt.show()
-
